4f0fd6966220ff50524b02512cb9e59287ac7f76
bmalvo <boydmalvo@gmail.com>
2024-11-03 20:36:49 +0100

    facerecognitionbrain init

---
+++ facerecognitionbrain/src/App.tsx
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+export class App extends Component {
+
+  render() {
+
+    return (
+
+      <div className="App">
+        <Navigation />
+        {/* <Logo />
+        <ImageLinkForm />
+        <FaceRecognition /> */}
+      </div>
+    )
+  }
+}
